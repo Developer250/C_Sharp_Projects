afd841ae1a98fa9b35117621971efba5b57dc5bd
Developer250 <jani.kekalainen@hotmail.com>
2024-08-14 19:21:00 +0300

    ffffff

---
+++ CS_Speech/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CS_Speech
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+            recognizer.SetInputToDefaultAudioDevice();
+            Grammar grammar = new DictationGrammar();
+            recognizer.LoadGrammar(grammar);
+            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            richTextBox1.Text += e.Result.Text + "\r\n";
+        }
+    }
+}
